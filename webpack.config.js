a404b1797bb9fce13e32187fc2d33d535d25ddeb
kabelo <kabelo@thutolwethu.com>
2018-06-11 03:22:43 +0200

    tried different config

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,25 @@
-var path = require('path');
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-
-  entry: './src/index.js', 
-  output: {
-    path: path.resolve(__dirname, 'build'), 
-    filename: 'index.js'
-  },
-  
-  module: {
-    rules: [
-        {
-            test: /\.js$/, 
-            include: path.resolve(__dirname, 'src'), 
-            exclude: /(node_modules|bower_components|build)/, 
-            use: {
+    entry: path.resolve(__dirname, 'src/lib/index.js'), 
+    output: {
+        path: path.resolve(__dirname, './dist/lib'), 
+        filename: 'index.js', 
+        library: '',
+        libraryTarget: 'commonjs'
+    }, 
+    externals: [nodeExternals()], 
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
                 loader: 'babel-loader', 
                 options: {
-                    presets: ['env']
+                    presets: ['@babel/preset-env', '@babel/preset-react']
                 }
             }
-        }
-    ]
-  }, 
-
-  externals: {
-    'react': 'commonjs react'
-  }
-
+        ]
+    }
 };
